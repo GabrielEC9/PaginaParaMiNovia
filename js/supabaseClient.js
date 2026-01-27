0ce8f895ab6733dc9917feb6da77c70808137945
GabrielEC9 <161523458+GabrielEC9@users.noreply.github.com>
2026-01-27 12:33:43 -0600

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // js/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const SUPABASE_URL = 'https://wunrnfpsjmlgjpllkgyu.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bnJuZnBzam1sZ2pwbGxrZ3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NzMzOTMsImV4cCI6MjA3ODA0OTM5M30.OnifOkyhTlFE8sYeU3mJF0qe5efQFoO05bC_h7hnqxk'
