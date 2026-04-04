3b8e76e65375ce74047e33cbcdbfbe5219f07f47
GabrielEC9 <161523458+GabrielEC9@users.noreply.github.com>
2026-04-04 12:55:47 -0600

    menu cambios

--- js/menu.js
+++ js/menu.js
@@ -50,13 +50,14 @@ overlay?.addEventListener('click', () => {
     { text: '💌 Frases', href: 'frases.html' },
     { text: '❓ Curiosidades', href: 'curiosidades.html' },
     { text: '🛍️ Tienda', href: 'tienda.html' },
-    { text: '🎁 Recompensas', href: 'recompensas.html' },
-    { text: '👀 🤔', href: 'chambeando.html' }
+    //{ text: '🎁 Recompensas', href: 'recompensas.html' },
+    { text: '🎁 Recompensas', href: 'chambeando.html' }
   ]
 
   if (role === 'admin') {
     links.push(
       { text: '✏️ Subir contenido', href: 'admin-form.html' },
+      { text: '🎁 Recompensas', href: 'recompensas.html' },
       { text: 'Pruebas', href: 'pruebas.html' },
       { text: '🔔 Notificaciones', href: 'notificaciones.html' }
     )
